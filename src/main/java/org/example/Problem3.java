f3990f177b3ac5e51f15ad13dee8cd37ec6a5207
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-07 22:20:18 +0500

    problem3 comments

--- src/main/java/org/example/Problem3.java
+++ src/main/java/org/example/Problem3.java
@@ -10,6 +10,11 @@ public class Problem3 {
         System.out.println(isPrime(n));
     }
 
+    /**
+     * Checks if a number is prime or composite.
+     * @param n The number to check.
+     * @return "Prime" if the number is prime, "Composite" if it is composite.
+     */
     public static String isPrime(int n) {
 
         for (int i = 2; i < n; i++) {
