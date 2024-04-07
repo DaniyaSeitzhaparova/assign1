5bf9b3eb1c84fe433c39bc32a3ffb0b7da602d18
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-07 21:24:16 +0500

    problem1

---
+++ src/main/java/org/example/Problem1.java
@@ -0,0 +1,28 @@
+package org.example;
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(MinimumElement(n, arr));
+    }
+
+   public static int MinimumElement(int n, int[] arr) {
+        int minElem = arr[0];
+
+       for (int i = 0; i < n; i++) {
+           if (arr[i] < minElem) {
+               minElem = arr[i];
+           }
+       }
+       return minElem;
+   }
+}
