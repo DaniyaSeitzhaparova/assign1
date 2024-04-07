656d1369e8d6d916619a4933416617da75c0ab88
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-07 22:43:40 +0500

    problem5 comments

--- src/main/java/org/example/Problem5.java
+++ src/main/java/org/example/Problem5.java
@@ -10,6 +10,11 @@ public class Problem5 {
         System.out.println(calculateFibonacci(n));
     }
 
+    /**
+     * Calculates the Fibonacci number at a given position in the sequence.
+     * @param n The position of the Fibonacci number to calculate.
+     * @return The Fibonacci number at the given position.
+     */
     public static int calculateFibonacci(int n) {
         if (n == 0 || n == 1) {
             return n;
