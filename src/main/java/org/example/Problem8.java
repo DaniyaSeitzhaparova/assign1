80189085f938148b763846f437170259b194aadf
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-09 15:19:07 +0500

    problem8

---
+++ src/main/java/org/example/Problem8.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+public class Problem8 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int n = s.length()-1;
+        if(checkString(s,n)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+
+    public static boolean checkString(String s,int n){
+        if(n==0){
+            return Character.isDigit(s.charAt(0));
+        }
+        else{
+            return Character.isDigit(s.charAt(n))&&checkString(s,n-1);
+        }
+    }
+}
